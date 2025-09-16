82f20dd2ccca13b1299dbf6795284c4f580cd288
EmilijaJ379 <emilija.jovanovic.19@singimail.rs>
2025-09-16 22:38:57 +0200

    Generated

---
+++ src/main/java/com/example/zavrsni_ispit_2025/ZavrsniIspit2025Application.java
@@ -0,0 +1,13 @@
+package com.example.zavrsni_ispit_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZavrsniIspit2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZavrsniIspit2025Application.class, args);
+	}
+ 
+}
---
+++ src/test/java/com/example/zavrsni_ispit_2025/ZavrsniIspit2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zavrsni_ispit_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZavrsniIspit2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
